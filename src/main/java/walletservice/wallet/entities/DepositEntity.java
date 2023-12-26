c0f5811d076fa6529acda5687724bbdb505bd7a4
mamad <you@gmail.com>
2023-12-26 15:55:05 +0330

    entities were created

--- src/main/java/walletservice/wallet/entities/DepositEntity.java
+++ src/main/java/walletservice/wallet/entities/DepositEntity.java
@@ -1,9 +1,6 @@
 package walletservice.wallet.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.*;
 
 import java.util.Date;
@@ -15,13 +12,6 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DepositEntity extends WalletTransactionEntity{
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-//    private Long senderCardNumber;
-//    private Long receiverCardNumber;
-//    private Integer cvv2;
-//    private Date expireDate;
 
 
 }
--- src/main/java/walletservice/wallet/entities/P2PEntity.java
+++ src/main/java/walletservice/wallet/entities/P2PEntity.java
@@ -1,9 +1,6 @@
 package walletservice.wallet.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.*;
 
 @Entity
@@ -13,7 +10,5 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class P2PEntity extends WalletTransactionEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+
 }
